8c28e65eca7d7c6c67ba9cb5dba7551d4a8d3fb8
Alan Ochoa <al8a12104@outook.com>
2022-03-20 18:12:46 -0600

    Update demo 8's main

--- m_demos/8-switch/oldmain.c
+++ m_demos/8-switch/oldmain.c
@@ -1,13 +1,12 @@
 #include <msp430.h>
 #include "libTimer.h"
 
-#define LED_RED BIT6    // P1.6
 #define LED_GREEN BIT0  // P1.0
+#define LED_RED   BIT6  // P1.6
 #define LEDS (BIT0 | BIT6)
 
 #define SW1 BIT3		    /* switch1 is p1.3 */
 #define SWITCHES SW1		/* only 1 switch on this board */
-
 
 void main(void) 
 {  
@@ -26,19 +25,35 @@ void main(void)
 
 
 
-static int button_down;
+static int buttonDown;
 static int state = 0;
 
-void update_state(int button_state)
-{  
-  if(button_down){                    
+void
+switch_interrupt_handler()
+{
+  char p1val = P1IN;		/* switch is in P1 */
+
+  /* update switch interrupt sense to detect changes from current buttons */
+  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
+  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
+
+  /* up = red, down = green */
+  if (p1val & SW1) {
+    buttonDown = 1;
+
+  } 
+  else{
+    buttonDown = 0;
+  }
+
+  if(buttonDown){                   /* PRESSED */
     switch(state){
-      case 0:                       /* GREEN ON | RED OFF */
+      case 0:                       
         P1OUT |= LED_GREEN;
         P1OUT &= ~LED_RED;
         state = 1;
         break;
-      case 1:                       /* GREEN OFF | RED ON */ 
+      case 1:                        
         P1OUT |= LED_RED;
         P1OUT &= ~LED_GREEN;
         state = 0;
@@ -49,14 +64,14 @@ void update_state(int button_state)
         state = 0; 
     }
   }
-  else if(!button_down){             /* UNPRESSED */ 
+  else if(!buttonDown){             /* UNPRESSED */ 
     switch(state){
-      case 0:                       /* GREEN ON | RED OFF */
+      case 0:                       
         P1OUT |= LED_GREEN;
         P1OUT &= ~LED_RED;
         state = 1;
         break;
-      case 1:                       /* GREEN OFF | RED ON */ 
+      case 1:                       
         P1OUT |= LED_RED;
         P1OUT &= ~LED_GREEN;
         state = 0;
@@ -71,26 +86,11 @@ void update_state(int button_state)
 
 
 
-void
-switch_interrupt_handler()
-{
-    char p1val = P1IN;		        /* switch is in P1 */
-
-    /* update switch interrupt sense to detect changes from current buttons */
-    P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-    P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-
-    button_down = p1val & SW1;    // button_state
-    update_state(button_down);
-}
-
-
-
 /* Switch on P1 (S2) */
 void
 __interrupt_vec(PORT1_VECTOR) Port_1(){
-    if (P1IFG & SWITCHES) {	        /* did a button cause this interrupt? */
-        P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
-        switch_interrupt_handler();	/* single handler for all switches */
-    }
+  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
 }
